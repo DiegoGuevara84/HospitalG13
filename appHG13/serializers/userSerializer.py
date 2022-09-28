cbc103cac10f9995e1381b2056cc4c23eb6b0ec8
diegoguevara84 <diegoguevara2006@gmail.com>
2022-09-28 16:02:36 -0500

    Update HospitalBE 6

--- appHG13/serializers/userSerializer.py
+++ appHG13/serializers/userSerializer.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 from appHG13.models.user import User
-from appHG13.models.account import Account
-from appHG13.serializers.accountSerializer import AccountSerializer
+#from appHG13.serializers.accountSerializer import AccountSerializer
 
 class UserSerializer(serializers.ModelSerializer):
-    account = AccountSerializer()
+    #account = AccountSerializer()
     class Meta:
         model = User
         fields = ['username', 'password', 'perfil', 'nombre', 'apellidos', 'telefono', 'genero']
