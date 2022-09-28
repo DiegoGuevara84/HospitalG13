f927d1575aa6a0d3e9de080841fd56e69d7030a6
diegoguevara84 <diegoguevara2006@gmail.com>
2022-09-28 14:43:03 -0500

    Update HospitalBE 5

--- projectHG13/settings.py
+++ projectHG13/settings.py
@@ -53,15 +53,14 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = {
-            'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-            'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-            'ROTATE_REFRESH_TOKENS': False,
+        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+        'ROTATE_REFRESH_TOKENS': False,
         'BLACKLIST_AFTER_ROTATION': True,
-            'UPDATE_LAST_LOGIN': False,
-
-            'ALGORITHM': 'HS256',
-            'USER_ID_FIELD': 'id',
-            'USER_ID_CLAIM': 'user_id',
+        'UPDATE_LAST_LOGIN': False,
+        'ALGORITHM': 'HS256',
+        'USER_ID_FIELD': 'id',
+        'USER_ID_CLAIM': 'user_id',
 }
 
 
@@ -169,5 +168,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#import django_heroku
-#django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
